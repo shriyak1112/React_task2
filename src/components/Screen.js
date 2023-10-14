a7227ab7027db500228e585a806130bc6ae6c253
shriyak1112 <shriyak1112@gmail.com>
2023-10-14 23:40:56 +0530

    sk

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { CalcContext } from '../context/CalcContext'
-import { Textfit } from 'react-textfit';
+
 const Screen = () => {
     const {calc } = useContext(CalcContext);
 
@@ -8,7 +8,7 @@ const Screen = () => {
 
 
   return (
-    <Textfit className="screen" max={70}  mode="single">{calc.num ? calc.num : calc.res}</Textfit>
+    <div className="screen" max={70}  mode="single">{calc.num ? calc.num : calc.res}</div>
 
   )
 }
